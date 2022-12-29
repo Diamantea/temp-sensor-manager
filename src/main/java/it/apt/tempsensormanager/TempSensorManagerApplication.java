0c7d4e6dc80208a31ac9b893f811a8c74b9dbdd9
Diamantea Mongelli <diamantea.mongelli@gmail.com>
2022-12-29 12:15:17 +0100

    Init Spring project

---
+++ src/main/java/it/apt/tempsensormanager/TempSensorManagerApplication.java
@@ -0,0 +1,13 @@
+package it.apt.tempsensormanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempSensorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempSensorManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/apt/tempsensormanager/TempSensorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.apt.tempsensormanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempSensorManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
